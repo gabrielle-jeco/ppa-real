8a4e6be9801a8315085ebd64985bb68a805865e9
unknown <gabrielle.christiando@binus.ac.id>
2026-06-09 09:36:30 +0700

    test

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t5qu2nct0v"
+    "revision": "0.i3gdtg3vul8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
